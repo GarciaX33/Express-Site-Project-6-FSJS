f4fa11305be74b7f17b2ab85ab987405002a0486
Daniel <danny.garcia80@hotmail.com>
2019-01-30 17:25:58 -0800

    will add error handlers to app file

--- app.js
+++ app.js
@@ -25,6 +25,18 @@ app.get('/projects:id', (req, res) => {
   });
 });
 
+app.use((req, res, next) => {
+  const err = new Error('Page Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status)
+  res.render('error');
+})
+
 app.listen(3000, () => {
   console.log("app is listening to localhost:3000")
 });
